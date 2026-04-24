3640c48483c88b207ac9f43dc909b5060278f4f0
Kaushal00Vaid <kaushal00vaid@gmail.com>
2026-04-24 22:57:18 +0530

    prisma and better-auth setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
-dotenv.config({ path: path.resolve(process.cwd(), "../.env") });
+dotenv.config();
 
 const app = express();
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL as string,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.get("/health", (req, res) => {
   return res.status(200).json({
---
+++ backend/src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+  trustedOrigins: [process.env.FRONTEND_URL as string],
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
